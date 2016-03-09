aa2e39c210db4511c940103a10128b6b4a73744d
Casey Cumbow <cumbow8@gmail.com>
2016-03-09 15:41:12 -0600

    it works!!

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,9 +2,13 @@ get '/' do
   erb :index
 end
 
-# post '/ocr' do
-#   erb :index
-# end
+post '/ocr' do
+	# use dotenv gem to set API_KEY
+	response = RestClient.post "http://api.ocrapiservice.com/1.0/rest/ocr", :language => 'en', apikey: ENV["API_KEY"], :image => File.new(params["image"][:tempfile], 'rb')
+
+  p response
+
+end
 
 
 
